7d85fb8ee296d00e39163453f30e460c1d19171a
Josep Ferrer <pepferrer.r@gmail.com>
2025-03-08 13:09:18 +0100

    Front end 1st upload with company landing page

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,13 +1,91 @@
 //import { useState } from "react";
 import "./App.css";
 import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 
 function App() {
   return (
     <>
-      <h1>Spare</h1>
-      <Button>Create new survey</Button>
-      <div className="bg-red-500 p-4 text-white">Tailwind is working!</div>
+      {/* Header Section */}
+      <div className="relative flex items-center w-full px-0 py-2 border-b">
+        {/* Left Side: Toggle Group */}
+        <div className="flex">
+          <ToggleGroup
+            type="single"
+            defaultValue="company"
+            className="space-x-0.5 border border-grey rounded-sm text-xs"
+          >
+            <ToggleGroupItem
+              value="company"
+              aria-label="Company View"
+              className="px-5 py-1 text-xs"
+            >
+              Company View
+            </ToggleGroupItem>
+            <ToggleGroupItem
+              value="user"
+              aria-label="User View"
+              className="px-2 py-1 text-xs"
+            >
+              User View
+            </ToggleGroupItem>
+          </ToggleGroup>
+        </div>
+
+        {/* Center: Logo (Always centered) */}
+        <h1 className="absolute left-1/2 transform -translate-x-1/2 text-lg font-bold">
+          Spare
+        </h1>
+
+        {/* Right Side: Connect Wallet Button */}
+        <div className="ml-auto">
+          <Button>Connect Wallet</Button>
+        </div>
+      </div>
+
+      {/* Dashboard Section */}
+      <div className="mt-6 px-4">
+        <Button>Create New Survey</Button>
+      </div>
+      {/* Table Section */}
+      <div className="px-30">
+        <Table className="mt-4">
+          <TableCaption>A list of all surveys created</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Title</TableHead>
+              <TableHead className="text-center">Completed</TableHead>
+              <TableHead className="text-center">Max Participants</TableHead>
+              <TableHead className="text-center">
+                Current Participants
+              </TableHead>
+              <TableHead className="text-right">Results</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium text-left">
+                Satisfaction
+              </TableCell>
+              <TableCell>No</TableCell>
+              <TableCell>2000</TableCell>
+              <TableCell className="text-center">23</TableCell>
+              <TableCell className="text-right px-0">
+                <Button className="px-2 py-1 text-xs">See results</Button>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
     </>
   );
 }
