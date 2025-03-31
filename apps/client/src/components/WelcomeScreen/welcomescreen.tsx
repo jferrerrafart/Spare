047805d5a3fd9e97a0526a7fafacb1e847196a7d
Josep Ferrer <pepferrer.r@gmail.com>
2025-03-31 16:30:59 +0200

    added own character at welcomepage

--- apps/client/src/components/WelcomeScreen/welcomescreen.tsx
+++ apps/client/src/components/WelcomeScreen/welcomescreen.tsx
@@ -1,12 +1,12 @@
 import rocket from "/home/josepferrer/BootCamp/Spare/my-turborepo/apps/client/src/utils/rocket.png";
-
+import rocketspareman from "/home/josepferrer/BootCamp/Spare2/Spare/apps/client/src/utils/roketspareman.png";
 function WelcomeScreen() {
   return (
-    <div className="flex flex-col items-center justify-center text-center py-40">
+    <div className="flex flex-col items-center justify-center text-center py-20">
       <p className="py-10 text-lg font-semibold">
         Please connect your wallet to start using SPARE
       </p>
-      <img src={rocket} alt="Rocket" className="h-35 mx-auto" />
+      <img src={rocketspareman} alt="Rocket" className="h-60 mx-auto" />
     </div>
   );
 }
